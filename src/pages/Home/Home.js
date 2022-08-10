9b2c051487b15e253af5fe1ea67f36bc8b2461ad
Trust Okerezi <trustokerezi42@gmail.com>
2022-08-11 00:15:23 +0100

    second project card

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import Hero from '../../components/Hero/Hero'
 import NavMenu from '../../components/Navigation/NavMenu'
 import BannerCard from '../../components/Project-Banner-Cards/BannerCard'
 import banner1 from '../../assets/banner.png'
+import banner2 from '../../assets/banner2.png'
 import './home.css'
 const Home = () => {
   return (
@@ -14,6 +15,12 @@ const Home = () => {
           type="UI UX DESIGN"
           title="ORO E- WALLET MOBILE APP"
           desc="Helping customers transfer and recieve money seamlessly, with real-time currency conversions to facilitate payments"
+        />
+        <BannerCard 
+          img={banner2}
+          type="UI UX DESIGN"
+          title="ATLAS AIRLINES"
+          desc="Improving user experience and ability to book flights easily"
         />
     </div>
   )
